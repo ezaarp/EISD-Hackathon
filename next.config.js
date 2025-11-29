58627a0e44ebf3b7dfb5d841ad6f24182e1b9c7c
ezaarp <andrariezarizqip@gmail.com>
2025-11-30 04:42:06 +0700

    Fix Deployment #4

--- next.config.js
+++ next.config.js
@@ -13,11 +13,11 @@ const nextConfig = {
       bodySizeLimit: '10mb',
     },
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  // eslint: {
+  //   // Warning: This allows production builds to successfully complete even if
+  //   // your project has ESLint errors.
+  //   ignoreDuringBuilds: true,
+  // },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
